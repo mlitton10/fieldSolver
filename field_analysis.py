6476266798558a2fc7ef5fb5cab14eab9b826bf9
mlitton10 <mattlitton10@gmail.com>
2025-12-09 15:50:51 -0800

    minor fixes

--- field_analysis.py
+++ field_analysis.py
@@ -8,16 +8,15 @@ def rungeKuttaBound(dydx, x0, y0, x_bound_low, x_bound_high, y_bound, h):
     y_list = [y0]
     x_list = [x0]
 
-    while x_bound_low < x0 < x_bound_high and y < y_bound:
+    while x_bound_low <= x0 <= x_bound_high and y <= y_bound:
         "Apply Runge Kutta Formulas to find next value of y"
-        k1 = h * dydx(x0, y)
-        k2 = h * dydx(x0 + 0.5 * h, y + 0.5 * k1)
-        k3 = h * dydx(x0 + 0.5 * h, y + 0.5 * k2)
-        k4 = h * dydx(x0 + h, y + k3)
+        k1 = h * dydx(x0, y)[0]
+        k2 = h * dydx(x0 + 0.5 * h, y + 0.5 * k1)[0]
+        k3 = h * dydx(x0 + 0.5 * h, y + 0.5 * k2)[0]
+        k4 = h * dydx(x0 + h, y + k3)[0]
 
         # Update next value of y
         y = y + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
-
         # Update next value of x
         x0 = x0 + h
 
@@ -32,12 +31,13 @@ class FieldLines:
         self.field_dict = fields
         self.Br = fields['Br']
         self.Bz = fields['Bz']
-        self.coordinate_system = self.coordinate_system
+        print(self.Bz.shape)
+        self.coordinate_system = coordinate_system
         self.r_wall = r_wall
 
-        field_ratio = self.Bz / self.Br
+        field_ratio = self.Br / self.Bz
 
-        self.ratio_interpolation = interp2d(*coordinate_system, field_ratio)
+        self.ratio_interpolation = interp2d(*coordinate_system, field_ratio.T)
         self.n_field_lines = n_field_lines
 
         self.spatial_bounds = self._find_spatial_bounds()
@@ -56,15 +56,14 @@ class FieldLines:
         z_initial = self.spatial_bounds[0][0]
         r_initial = np.linspace(0, self.r_wall, self.n_field_lines)
 
-        return np.array([(z_initial, r) for r in r_initial])
+        return np.array([(z_initial+0.001, r) for r in r_initial])
 
     def solveFieldLines(self):
-
         solutions_set = []
 
         for ic in self.initial_conditions:
-            z_sol, r_sol = rungeKuttaBound(self.ratio_interpolation, ic[0], ic[1], self.spatial_bounds[0][0], self.spatial_bounds[0][1],
-                                           self.spatial_bounds[1][1], self.stepsize)
+            z_sol, r_sol = rungeKuttaBound(self.ratio_interpolation, ic[0], ic[1], self.spatial_bounds[0][0],
+                                           self.spatial_bounds[0][1], self.spatial_bounds[1][1], self.stepsize)
 
             solutions_set.append((z_sol, r_sol))
         return solutions_set
