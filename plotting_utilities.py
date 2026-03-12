f309a0d7a974ff45617e4ee40bfd301f5e75f3eb
mlitton10 <mattlitton10@gmail.com>
2026-03-12 16:00:08 -0700

    Adjustment to vessel boundary linewidth for better visibility.

--- plotting_utilities.py
+++ plotting_utilities.py
@@ -6,7 +6,7 @@ def plot_machine_boundary(f, a, r_wall, z_wall):
     z_range = np.linspace(z_wall[0], z_wall[1], 10, endpoint=True)
     r_range = np.linspace(0, r_wall, 10, endpoint=True)
 
-    a.plot([z_wall[0]] * 10, r_range, color='k', ls='-')
-    a.plot([z_wall[1]] * 10, r_range, color='k', ls='-')
-    a.plot(z_range, [r_wall] * 10, color='k', ls='-')
+    a.plot([z_wall[0]] * 10, r_range, color='k', ls='-', lw=1.5)
+    a.plot([z_wall[1]] * 10, r_range, color='k', ls='-', lw=1.5)
+    a.plot(z_range, [r_wall] * 10, color='k', ls='-', lw=1.5)
     return f, a
